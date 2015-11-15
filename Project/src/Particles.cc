bb9bb818ef870bf9a3a576b60601152fc96c97a4
Conrad Wahlén <Conrad.Wahlen@gmail.com>
2015-11-15 23:08:00 +0100

    Removed the transform feedback code and cleaned up a little

--- Project/src/Particles.cc
+++ Project/src/Particles.cc
@@ -11,33 +11,23 @@ Particles::Particles(GLuint numParticles, GLfloat initRadius) {
 	setParticles = numParticles;
 	particles = setParticles*setParticles*setParticles;
 	drawParticles = 0;
+	startMode = 1;
 	radius = initRadius;
 	oldT = 0;
+
+	doUpdate = false;
+	renderModels = false;
+
 	numBins = 16;
 	bins = numBins*numBins*numBins;
 	binSize = 4.0f;
 	displaybin = 0;
-	startMode = 1;
-	currVAO = 0;
-	currTFB = 1;
-	doUpdate = false;
-	renderModels = false;
-
+	
 	inBufferIndex = 0;
 	outBufferIndex = 1;
 
 	prefixArrayIn = (GLuint*)malloc(sizeof(GLuint) * bins);
 	prefixArrayOut = (GLuint*)malloc(sizeof(GLuint) * bins);
-
-	// Create the query ID
-	//glGenQueries(1, &qId);
-	// Generate Buffers
-	//glGenBuffers(2, updateBuffer);
-	//glGenBuffers(1, &cullingBuffer);
-	// Generate VAO's
-	//glGenVertexArrays(2, updateVAO);
-	//glGenVertexArrays(2, cullingVAO);
-
 
 	//create buffers
 	glGenBuffers(3, particleBuffers);
@@ -46,8 +36,6 @@ Particles::Particles(GLuint numParticles, GLfloat initRadius) {
 }
 
 bool Particles::Init(Camera* setCam) {
-	//InitGLStates();
-
 	SetParticleData();
 
 	CompileComputeShader(&computeBin, "src/shaders/bin.comp");
@@ -272,165 +260,6 @@ void Particles::DoCompute(GLfloat t) {
 	glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, particleBuffers[outBufferIndex]);
 
 	printError("Do Compute: Swap in/out buffer");
-}
-
-void Particles::InitGLStates() {
-	// Create program for the update and culling TFB
-	updateShader = loadShaders("src/shaders/update.vert", NULL);
-	cullShader = loadShadersG("src/shaders/culling.vert", NULL, "src/shaders/culling.geom");
-
-	// Names for transform feedback return values
-	const GLchar* updateVaryings[] = {"updatePosValue", "updateVelValue", "updateAccValue"};
-	const GLchar* cullingVaryings[] = {"culledPosValue"};
-
-	// Create the transform feedback output variable
-	glTransformFeedbackVaryings(updateShader, 3, updateVaryings, GL_INTERLEAVED_ATTRIBS);
-	glTransformFeedbackVaryings(cullShader, 1, cullingVaryings, GL_INTERLEAVED_ATTRIBS);
-
-	// Link program, we do this after creating buffers since the linking depends on
-	// knowledge about the output
-	glLinkProgram(updateShader);
-	printProgramInfoLog(updateShader, "Transform Feedback", NULL, NULL, NULL, NULL);
-	printError("Link update shader");
-
-	glLinkProgram(cullShader);
-	printProgramInfoLog(cullShader, "Transform Feedback", NULL, NULL, NULL, NULL);
-	printError("Link cull shader");
-
-	// Create a culling buffer
-	glBindBuffer(GL_ARRAY_BUFFER, cullingBuffer);
-	glBufferData(GL_ARRAY_BUFFER, particles * 3 * sizeof(GLfloat), NULL, GL_STATIC_COPY);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-	// Print errors
-	printError("Create Culling Buffer");
-
-	// Set VAOs
-	// Get attributes
-	GLint inPosAttr = glGetAttribLocation(updateShader, "posValue");
-	GLint inVelAttr = glGetAttribLocation(updateShader, "velValue");
-	GLint inAccAttr = glGetAttribLocation(updateShader, "accValue");
-	GLint upPosAttr = glGetAttribLocation(cullShader, "posValue");
-
-	for (int i = 0; i < 2; i++) {
-		glBindBuffer(GL_ARRAY_BUFFER, updateBuffer[i]);
-		glBufferData(GL_ARRAY_BUFFER, particles * 9 * sizeof(GLfloat), particleData.data(), GL_STATIC_COPY);
-
-		// Set update VAO states
-		glBindVertexArray(updateVAO[i]);
-
-		glEnableVertexAttribArray(inPosAttr);
-		glVertexAttribPointer(inPosAttr, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 9, 0);
-		glEnableVertexAttribArray(inVelAttr);
-		glVertexAttribPointer(inVelAttr, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 9, (void*)(3 * sizeof(GLfloat)));
-		glEnableVertexAttribArray(inAccAttr);
-		glVertexAttribPointer(inAccAttr, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 9, (void*)(6 * sizeof(GLfloat)));
-
-		glBindVertexArray(0);
-
-		// Set culling VAO state
-		glBindVertexArray(cullingVAO[i]);
-
-		glEnableVertexAttribArray(upPosAttr);
-		glVertexAttribPointer(upPosAttr, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 9, 0);
-
-		glBindVertexArray(0);
-
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
-	}
-
-	printError("Create Transform Feedback VAOs");
-}
-
-void Particles::Update(GLfloat t) {
-	GLfloat deltaT = t - oldT;
-
-	model->Update(t);
-	billboard->Update(t);
-
-	glUseProgram(updateShader);
-	glUniform1f(glGetUniformLocation(updateShader, "deltaT"), deltaT);
-	//glUniform1f(glGetUniformLocation(updateShader, "t"), t);
-
-	printError("Particles Update");
-	if (doUpdate && particles > 0) {
-		// Disable rasterizer since we dont want to draw
-		glEnable(GL_RASTERIZER_DISCARD);
-
-		// Set the attributes for the first pass
-		glBindVertexArray(updateVAO[currVAO]);
-		glBindBufferBase(GL_TRANSFORM_FEEDBACK_BUFFER, 0, updateBuffer[currTFB]);
-
-			// Enter transform feedback mode
-			glBeginTransformFeedback(GL_POINTS);
-
-				// Perform the transform feedback
-				glDrawArrays(GL_POINTS, 0, particles);
-
-			// End transform feedback mode
-			glEndTransformFeedback();
-
-		// Enable the rasterizer again
-		glDisable(GL_RASTERIZER_DISCARD);
-
-		glBindBuffer(GL_TRANSFORM_FEEDBACK_BUFFER, 0);
-		glBindVertexArray(0);
-
-		printError("Do Update");
-	}
-
-	oldT = t;
-}
-
-void Particles::Cull() {
-	glUseProgram(cullShader);
-
-	glUniform3fv(glGetUniformLocation(cullShader, "boxNormals"), 5, cam->GetCullingNormals());
-	glUniform3fv(glGetUniformLocation(cullShader, "boxPoints"), 5, cam->GetCullingPoints());
-	glUniform1f(glGetUniformLocation(cullShader, "radius"), radius);
-
-	printError("Particles Before Cull");
-	
-	if (particles > 0) {
-		// Disable rasterizer since we dont want to draw
-		glEnable(GL_RASTERIZER_DISCARD);
-
-		// Set the attributes for the second pass
-		glBindVertexArray(cullingVAO[currTFB]);
-		glBindBufferBase(GL_TRANSFORM_FEEDBACK_BUFFER, 0, cullingBuffer);
-
-		// Enter transform feedback mode
-		glBeginTransformFeedback(GL_POINTS);
-
-			// Start recording query
-			glBeginQuery(GL_PRIMITIVES_GENERATED, qId);
-
-				// Perform the transform feedback
-				glDrawArrays(GL_POINTS, 0, particles);
-
-			// End Query
-			glEndQuery(GL_PRIMITIVES_GENERATED);
-
-		// End transform feedback mode
-		glEndTransformFeedback();
-
-		// Enable the rasterizer again
-		glDisable(GL_RASTERIZER_DISCARD);
-
-		// Get number of culled objects
-		glGetQueryObjectuiv(qId, GL_QUERY_RESULT, &drawParticles);
-
-		glBindBuffer(GL_TRANSFORM_FEEDBACK_BUFFER, 0);
-		glBindVertexArray(0);
-
-		printError("Do Culling");
-	}
-
-	// Swap TFB buffer to read and write
-	if (doUpdate) {
-		currVAO = currTFB;
-		currTFB = 1 - currVAO;
-	}
 }
 
 void Particles::Draw() {
--- Project/src/Particles.h
+++ Project/src/Particles.h
@@ -17,13 +17,6 @@ struct ParticleStruct {
 
 class Particles {
 private:
-	// Transform feedback
-	GLuint updateVAO[2], cullingVAO[2];
-	GLuint updateBuffer[2], cullingBuffer;
-	GLuint updateShader, cullShader;
-	GLuint qId;
-	GLint currVAO, currTFB;
-
 	// Particle info
 	GLfloat radius;
 	GLuint startMode;
@@ -47,7 +40,6 @@ private:
 
 	// Methods
 	void SetParticleData();
-	void InitGLStates();
 
 	// Compute shader stuff
 	GLuint particleBuffers[3], binBuffers[2], counterBuffer;
@@ -62,10 +54,7 @@ public:
 
 	bool Init(Camera* setCam);
 	void DoCompute(GLfloat t);
-	void Update(GLfloat t);
-	void Cull();
 	void Draw();
-
 
 	void ToggleDrawModels() { renderModels = !renderModels; }
 	void ToggleUpdate() { doUpdate = !doUpdate; }
@@ -77,8 +66,6 @@ public:
 	const GLint GetParticles() { return particles; }
 	const GLint GetDrawParticles() { return drawParticles; }
 	const GLint GetSetParticles() { return setParticles; }
-	const GLuint GetUpdateProgram() { return updateShader; }
-	const GLuint GetCullingProgram() { return cullShader; }
 
 	void SetParticles(GLuint newParticles);
 	void SetParticles(GLuint newParticles, GLuint newType);
